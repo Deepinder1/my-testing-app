e93574724ea1a16a6cba5e47a89efe43fc767a0f
Deepinder Singh <deep6421@gmail.com>
2021-03-31 09:32:53 +0530

    changes

--- screens/AddScreen.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import NotImplemented from '../NotImplemented'
-
-export default class AddScreen extends Component {
-  render() {
-    return <NotImplemented />
-  }
-}
--- screens/FavoriteScreen.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import NotImplemented from '../NotImplemented'
-
-export default class FavoriteScreen extends Component {
-  render() {
-    return <NotImplemented />
-  }
-}
